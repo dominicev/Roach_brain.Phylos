a2cca79eddffa18e01a616e616a6861130092b56
Dominic Evangelista <dominicev@gmail.com>
2023-08-10 12:46:58 -0500

    update tree functions

--- AwareSupport/node support transfering.R
+++ AwareSupport/node support transfering.R
@@ -3,14 +3,23 @@
 library(ape)
 library(phangorn)
 library(phytools)
-library(adephylo)
-library(TreeDist)
 library(dplyr)
 library(TreeTools)
-library(RRphylo)
-library(devtools)
 library(ips)
 library(combinat)
+
+####Unused packages####
+  #library(adephylo)
+  #library(TreeDist)
+  #library(RRphylo)
+  #library(devtools)
+  #library(funchir)
+
+
+  #Unused packages determined by...
+    #funchir::stale_package_check('B:/OneDrive - University of Illinois - Urbana/Science/R programs/Roach_brain_Phylos/AwareSupport/node support transfering.R')
+                   
+                   
 source("B:/OneDrive - University of Illinois - Urbana/Science/R programs/Roach_brain_Phylos/tree functions.R") #use this to load the dependency script locally
 #devtools::source_url("https://github.com/dominicev/Roach_brain_Phylos/blob/main/tree%20functions.R")
 
--- tree functions.R
+++ tree functions.R
@@ -4,14 +4,20 @@ library(phangorn)
 library(phytools)
 library(adephylo)
 library(fitdistrplus)
+library(TreeSearch)
 library(reshape2)
 library(dplyr)
-library(rlist)
 library(MonoPhy)
 library(data.table)
 library(foreach)
 library(doParallel)
 library(TreeTools)
+library(castor)
+
+###Unused packages###
+#library(rlist)
+    #Unused packages determined by...
+    #funchir::stale_package_check('B:/OneDrive - University of Illinois - Urbana/Science/R programs/Roach_brain_Phylos/tree functions.R')
 
 #source("B:\OneDrive - University of Illinois - Urbana\Science\R programs\Roach_brain_Phylos\tree functions.R")
  #use this to load this file into an r book
@@ -37,8 +43,20 @@ rootIncompleteTree<-function(phylo, outGroupList) {
   
 }
 
-###removing taxa that have extremely large pairwise distances
-###
+
+# Median Absolute Deviation (MAD) Method for dropping extremely outliers; FROM GPT4
+remove_mad_outliers <- function(x, multiplier = 3.5) {
+  mad <- mad(x, constant = 1)
+  med <- median(x)
+  
+  # Identify outliers
+  outliers <- abs(x - med) / mad > multiplier
+  
+  # Return data without outliers
+  return(x[!outliers])
+}
+
+###removing taxa that have extremely large pairwise distances###
 ###This will compare the pairwise tip distances in the whole tree to those generated under an estimated gamma distribution and remove those that appear to have numerous cases (relative to the total number of taxa) where they are outside the alpha (default 95%) quantile of that distribution.
 ###
 #troubelshooting
@@ -51,17 +69,22 @@ rootIncompleteTree<-function(phylo, outGroupList) {
 #randomSeed = 2345
 
 
-trimPairTipOutliers<-function(tree, alpha = 0.95, relAbund = "Default", randomSeed = 2345 ) {
+trimPairTipOutliers<-function(tree, alpha = 0.95, relAbund = "Default", randomSeed = 2345, fitdistsMethod = "mle" ) {
       ##Import tree
       phylo<-tree
       #plot.phylo(phylo)
       
-      #Define the number of times the taxon name has to appear in list of long branch taxa to be considered an true long branch taxa
+      #Define the number of times the taxon name has to appear in list of long branch taxa to be considered a true long branch taxa
       if(relAbund=="Default"){relAbund<-(length(phylo[[4]])/4)}
       
       #calculate root to tip branch length distances
-      dists<-distTips(phylo, tips = "all", method = "patristic")
+      #dists<-distTips(phylo, tips = "all", method = "patristic") #old method/slow
+      dists<-get_all_pairwise_distances(phylo, only_clades = phylo$tip.label)
       
+      rownames(dists)<-phylo$tip.label
+      colnames(dists)<-phylo$tip.label
+      dists<-as.dist(dists)
+
       #reshape dists into a list
       df <- melt(as.matrix(dists), varnames = c("row", "col"))
       allDists<-df$value
@@ -69,7 +92,7 @@ trimPairTipOutliers<-function(tree, alpha = 0.95, relAbund = "Default", randomSe
       
       #fit gamma distribution to distances
       set.seed(randomSeed)
-      fit.gamma = fitdist(allDists[allDists>0], distr = "gamma", method = "mle")
+      fit.gamma = fitdist(allDists[allDists>0], distr = "gamma", method = fitdistsMethod)
       #fit.gamma[1]
       
       #Calculate outliers based on random sampling of gamma distribution
@@ -78,13 +101,27 @@ trimPairTipOutliers<-function(tree, alpha = 0.95, relAbund = "Default", randomSe
       #shortBranches<-quantile(randomGammaValues, c(.001))
       longBranches<-quantile(randomGammaValues, c(alpha))
       
-      rowNames<-df$row; colNames<-df$col
-      longBranchTaxa<-unlist(c(rowNames[df$value>longBranches],colNames[df$value>longBranches]))
-      frequencies<-sort(table(longBranchTaxa), decreasing = TRUE)
-      strictLBTaxa<-dimnames(frequencies[frequencies>relAbund])
+      
+      ####SECTION BELOW WORKS BUT MIGHT BE TOO SLOW
+      #rowNames<-df$row; colNames<-df$col
+      #longBranchTaxa<-unlist(c(rowNames[df$value>longBranches],colNames[df$value>longBranches]))
+      #frequencies<-sort(table(longBranchTaxa), decreasing = TRUE)
+      #strictLBTaxa<-dimnames(frequencies[frequencies>relAbund])
+      
       #delete outlier tips and export
       
-      ifelse(length(strictLBTaxa)>0,phylo<-drop.tip(  phylo,strictLBTaxa$longBranchTaxa ), 0)#redefines the phylogeny
+      #ifelse(length(strictLBTaxa)>0,phylo<-drop.tip(  phylo,strictLBTaxa$longBranchTaxa ), 0)#redefines the phylogeny
+      
+      #####SECTION BELOW IS THE REWRITTEN PART FROM GPT4
+      longBranchIndices <- which(df$value > longBranches)
+      longBranchTaxa <- unlist(c(df$row[longBranchIndices], df$col[longBranchIndices]))
+      
+      frequencies <- sort(table(longBranchTaxa), decreasing = TRUE)
+      strictLBTaxa <- dimnames(frequencies[frequencies > relAbund])
+      
+      # Delete outlier tips and redefine the phylogeny
+      ifelse(length(longBranchTaxa) > 0, phylo<-drop.tip(phylo, strictLBTaxa$longBranchTaxa), 0)
+      
       
       #write.tree(phylo, paste(strsplit(files, ".", fixed=TRUE)[[1]][[1]],".tiptrimmed.tre", sep=""))
       print(paste(length(strictLBTaxa$longBranchTaxa), "tips dropped from tree"))
@@ -95,50 +132,59 @@ trimPairTipOutliers<-function(tree, alpha = 0.95, relAbund = "Default", randomSe
     
 }
 
-###removing taxa that have extremely large pairwise distances
-###
+###removing taxa that have extremely large root-to-tip distances###
 ###This will compare the pairwise tip distances in the whole tree to those generated under an estimated gamma distribution and remove those that appear to have numerous cases (relative to the total number of taxa) where they are outside the alpha (default 95%) quantile of that distribution.
 ###
+### rooting option will take a list of rooting taxa in the order of priority (i.e., the input for rootIncompleteTree)
 
-
+#outgroupTaxonList<-rooting<-outgroupList
 ####this is the same as the above function but it operates on a single tree at a time
-
-trimRootTipOutliers<-function(tree, alpha = 0.99, method = "Default", randomSeed = 2345) {
+trimRootTipOutliers<-function(tree, alpha = 0.99, rooting = "Default", randomSeed = 2345, fitdistsMethod = "mle") {
 
     ##Import tree
     phylo<-tree
-    #plot.phylo(phylo)
-    
+    outgroupTaxonList<-rooting
+  
     #root tree
-    ifelse(method == "Default", phylo<-midpoint.root(phylo), phylo<-rootIncompleteTree(phylo, outgroupsOrdered))
+    ifelse(rooting == "Default", phylo<-midpoint.root(phylo), phylo<-rootIncompleteTree(phylo, outgroupTaxonList))
     
     #calculate root to tip branch length distances
-    dists=distRoot(phylo, tips = "all", method = "patristic")
+    #dists=distRoot(phylo, tips = "all", method = "patristic") #old method/slow
+    dists<-get_all_distances_to_root(phylo)[1: length(phylo$tip.label)]
+    names(dists)<-phylo$tip.label
     
     #fit gamma distribution to distances
     set.seed(randomSeed)
-    fit.gamma = fitdist(dists, distr = "gamma", method = "mle")
-    fit.gamma[1]
+    
+    # Remove outliers in dists so the function does a better job of dropping most on the first run
+    #dists_no_outliers <- remove_mad_outliers(dists)
+    
+    fit.gamma = fitdist(dists, distr = "gamma", method = fitdistsMethod)
     
     #Calculate outliers based on random sampling of gamma distribution
     randomGammaValues<-rgamma(9999, fit.gamma$estimate[[1]], rate = fit.gamma$estimate[[2]] )
     
-    #shortBranches<-quantile(randomGammaValues, c(.001))
     longBranches<-quantile(randomGammaValues, c(alpha))
     
     #delete outlier tips and export
-    { 
-      longBranchTaxa<-c()
-      j=0;k=0
-      for(value in dists){
-        j=j+1;
-        name=names(dists[j])
-        if(value>longBranches[[1]]){k=k+1;
-        longBranchTaxa[k]<-name}
-      }
-    }###gets the taxon names of outliers
-    longBranchTaxa
-    ifelse(length(longBranchTaxa)>0,phylo<-drop.tip(phylo,longBranchTaxa ), 0)#redefines the phylogeny
+    #{ 
+    #  longBranchTaxa<-c()
+    #  j=0;k=0
+    #  for(value in dists){
+    #    j=j+1;
+    #    name=names(dists[j])
+    #    if(value>longBranches[[1]]){k=k+1;
+    #    longBranchTaxa[k]<-name}
+    #  }
+    
+    #}###gets the taxon names of outliers
+    
+    ###ABOVE is the old method, which works but is slow. Below is the new method which was written by GPT4
+    longBranchIndices <- which(dists > longBranches[[1]])
+    longBranchTaxa <- names(dists[longBranchIndices])
+    
+    #longBranchTaxa
+   ifelse(length(longBranchTaxa) > 0, phylo<-drop.tip(phylo, longBranchTaxa), 0) #redefines the phylogeny
     
     #write.tree(phylo, paste(strsplit(files, ".", fixed=TRUE)[[1]][[1]],".tiptrimmed.tre", sep=""))
     print(paste(length(longBranchTaxa), "tips dropped from tree"))
@@ -151,11 +197,14 @@ trimRootTipOutliers<-function(tree, alpha = 0.99, method = "Default", randomSeed
 ###recursive tip trimming function
 
 
-recursiveTrimOutlierTaxa<-function(tree){
+recursiveTrimOutlierTaxa<-function(tree, alpha = 0.999, rooting = "Default", randomSeed = 2345, fitdistsMethod = "mle"){
   phylo<-tree
+  
   repeat{
     previousTaxaLength<-length(phylo$tip.label)
-    phylo<-trimRootTipOutliers(trimPairTipOutliers(phylo))
+    phylo<-trimRootTipOutliers(
+      trimPairTipOutliers(phylo, alpha = alpha, relAbund = "Default", randomSeed = randomSeed, fitdistsMethod = fitdistsMethod )
+      , alpha = alpha, rooting = rooting, randomSeed= randomSeed, fitdistsMethod = fitdistsMethod)
     currentTaxonLength<-length(phylo$tip.label)
     if(currentTaxonLength==previousTaxaLength){break()}
   }
@@ -499,3 +548,75 @@ concatNodeLabels<-function(finalTree, sourceTree){
 }
 
 
+
+###This is a custom function that evaluates input that looks like Mathematica's Table function and provides a similar output style, but in R!
+
+tableW <- function(expr, var_list) {
+  # Extract the variable, start, end, and increment
+  var_name <- var_list[[1]]
+  start <- var_list[[2]]
+  end <- var_list[[3]]
+  increment <- ifelse(length(var_list) > 3, var_list[[4]], 1)
+  
+  # Create a vector to store the results
+  result <- vector("list", length = (end - start) / increment + 1)
+  
+  # Evaluate the expression for each value of the variable
+  for (i in seq(start, end, by = increment)) {
+    assign(var_name, i, envir = .GlobalEnv)
+    result[[i - start + 1]] <- eval(parse(text = expr))
+  }
+  
+  # Return the result as a matrix or array if possible
+  return(do.call(rbind, result))
+}
+
+# Example usage
+expr <- "x^2"
+var_list <- list("x", 1, 5)
+result <- tableW(expr, var_list)
+print(result)
+
+
+
+
+###Match tips between two trees with different tip sets. GPT-4
+reciprocalTreeTrim <- function(tree1, tree2) {
+  # Find the common tips
+  common_tips <- intersect(tree1$tip.label, tree2$tip.label)
+  
+  # Find the tips to drop from each tree
+  drop_tips_tree1 <- setdiff(tree1$tip.label, common_tips)
+  drop_tips_tree2 <- setdiff(tree2$tip.label, common_tips)
+  
+  # Drop the extra tips from each tree
+  if (length(drop_tips_tree1) > 0) {
+    tree1 <- drop.tip(tree1, drop_tips_tree1)
+  }
+  if (length(drop_tips_tree2) > 0) {
+    tree2 <- drop.tip(tree2, drop_tips_tree2)
+  }
+  
+  # Return the trimmed trees
+  return(list(tree1, tree2))
+}
+
+
+#this function is a wrapper for TreeSearch's SPRMoves. From GPT4
+rSPRNew <- function(tree, N) {
+  # Perform up to N SPR moves
+  for (i in 1:N) {
+    # Perform a single SPR move
+    tree <- SPRMoves(tree)
+    
+    # Force the tree to be binary
+    tree <- multi2di(collapse.singles(tree))
+    
+    # Optional: Check if the tree is binary (should always be true)
+    #if (!is.binary(tree)) {
+    #  stop("Tree is not binary after move", i)
+    #}
+  }
+  
+  return(tree)
+}
